813d7e201e586bf23a3d5438a7cb23d1883f5a19
Skylan Lew <skylanslew@gmail.com>
2022-08-30 13:17:47 -0700

    write test 3 for Sudoku
    Co-authored-by: Harold Mesa <harold.mesa@icloud.com>
    
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -28,9 +28,20 @@ describe('Sudoku', () => {
     expect(sudoku.row9).toEqual([5, 7, 4, 9, 6, 8, 2, 3, 1]);
   });
 
-  test('should correctly create columns from the arrays in the rows', ()=>{
-    expect(sudoku.column1).toEqual([row1[0],row2[0],row3[0],row4[0],row5[0],row6[0],row7[0],row8[0],row9[0]]);
+  test('should correctly create column1 from the arrays in the rows', ()=> {
+    expect(sudoku.column1).toEqual([row1[0], row2[0], row3[0], row4[0], row5[0], row6[0], row7[0], row8[0], row9[0]]);
   });
 
+  test('should correctly create 9 columns from the arrays in the 9 rows', ()=> {
+    expect(sudoku.column1).toEqual([row1[0], row2[0], row3[0], row4[0], row5[0], row6[0], row7[0], row8[0], row9[0]]);
+    expect(sudoku.column2).toEqual([row1[1],row2[1],row3[1],row4[1],row5[1],row6[1],row7[1],row8[1],row9[1]]);
+    expect(sudoku.column3).toEqual([row1[2],row2[2],row3[2],row4[2],row5[2],row6[2],row7[2],row8[2],row9[2]]);
+    expect(sudoku.column4).toEqual([row1[3],row2[3],row3[3],row4[3],row5[3],row6[3],row7[3],row8[3],row9[3]]);
+    expect(sudoku.column5).toEqual([row1[4],row2[4],row3[4],row4[4],row5[4],row6[4],row7[4],row8[4],row9[4]]);
+    expect(sudoku.column6).toEqual([row1[5],row2[5],row3[5],row4[5],row5[5],row6[5],row7[5],row8[5],row9[5]]);
+    expect(sudoku.column7).toEqual([row1[6],row2[6],row3[6],row4[6],row5[6],row6[6],row7[6],row8[6],row9[6]]);
+    expect(sudoku.column8).toEqual([row1[7],row2[7],row3[7],row4[7],row5[7],row6[7],row7[7],row8[7],row9[7]]);
+    expect(sudoku.column9).toEqual([row1[8],row2[8],row3[8],row4[8],row5[8],row6[8],row7[8],row8[8],row9[8]]);
+  });
 });
 
